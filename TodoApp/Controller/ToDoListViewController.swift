058a95a51678dcbfd516954aee87f010dee8b52b
Shubham Gupta <sgshubh157@gmail.com>
2020-01-20 20:01:40 +0530

    Convert to MVC design

--- TodoApp/Controller/ToDoListViewController.swift
+++ TodoApp/Controller/ToDoListViewController.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    var itemArray = ["Find Mike","Buy Eggos","Destroy Demogorgon"]
+    var itemArray = [Item]()
     let defaults = UserDefaults()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let  items = defaults.array(forKey: "ToDoListArray") as? [String]{
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy Demogorgon"
+        itemArray.append(newItem3)
+        
+        
+        
+        
+        
+        if let  items = defaults.array(forKey: "ToDoListArray") as? [Item]{
             itemArray = items
         }
         // Do any additional setup after loading the view.
@@ -24,7 +43,9 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done  ? .checkmark : .none
         return cell
     }
     
@@ -48,14 +69,16 @@ class ToDoListViewController: UITableViewController {
     }
     
     
-   //MARK - Add new items
+    //MARK - Add new items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField  = UITextField()
         let alert = UIAlertController(title: "Add New Todo Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
             
@@ -63,7 +86,7 @@ class ToDoListViewController: UITableViewController {
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
-           
+            
         }
         alert.addAction(action)
         present(alert,animated: true,completion: nil)
---
+++ TodoApp/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  TodoApp
+//
+//  Created by Shubham Gupta on 20/01/20.
+//  Copyright © 2020 Shubham Gupta. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
